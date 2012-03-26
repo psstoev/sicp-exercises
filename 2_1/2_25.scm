570c0489e56dce9d3778f38acb718b5e998058ee
Plamen Stoev <psstoev@gmail.com>
2012-03-26 18:29:57 +0300

    Exercise 2.25 - combinations of "car" and "cdr".
    It's curious how seemingly easy tasks require careful thinking. I
    managed to get the right answer without help from the interpreter only
    on the second example (the easiest).

---
+++ 2_1/2_25.scm
@@ -0,0 +1,14 @@
+;; Exercise 2.25: Give combinations of cars and cdrs that will pick 7
+;; from each of the following lists:
+;; (1 3 (5 7) 9)
+;; ((7))
+;; (1 (2 (3 (4 (5 (6 7))))))
+
+;; Answer:
+
+;; (1 3 (5 7) 9): (car (cdr (car (cdr (cdr l)))))
+
+;; ((7)): (car (car x))
+
+;; (1 (2 (3 (4 (5 (6 7)))))):
+;; (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr l))))))))))))
