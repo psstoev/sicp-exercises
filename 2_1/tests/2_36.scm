4ab901d96a268becd1e7eb57e218847347203251
Plamen Stoev <psstoev@gmail.com>
2012-03-31 09:15:28 +0300

    Exercise 2.36 - "accumulate-n"
    I like this very much. It very short, very simple, and you can use it
    to define n-argument map.

---
+++ 2_1/tests/2_36.scm
@@ -0,0 +1,17 @@
+;; Tests for exercise 2.33:
+
+;; Load the test manager:
+(load "../../lib/test-manager/load.scm")
+;; Load the exercise:
+(load "../2_36.scm")
+
+(define s '((1 2 3) (4 5 6) (7 8 9) (10 11 12)))
+;; Define tests:
+(in-test-group 
+ ex-2.36
+
+ (define-each-check
+   (equal? (accumulate-n + 0 s) '(22 26 30)))
+)
+
+(run-registered-tests)
